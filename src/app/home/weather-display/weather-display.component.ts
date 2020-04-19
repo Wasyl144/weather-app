0b7f06768a0191ea5cc8640c7b3a3b87fc4d9e45
Wasyl <wasyl144@outlook.com>
2020-04-19 12:04:42 +0200

    Some improvments, UI update

--- src/app/home/weather-display/weather-display.component.ts
+++ src/app/home/weather-display/weather-display.component.ts
@@ -3,6 +3,8 @@ import { WeatherService } from '../weather.service';
 import { WeatherModel } from '../weather.model';
 import { Observable } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-weather-display',
@@ -24,7 +26,8 @@ export class WeatherDisplayComponent implements OnInit {
     wind_speed: 0,
     cod:0,
   };
-  constructor(private weatherService: WeatherService) {
+  constructor(private weatherService: WeatherService,
+    private _snackBar: MatSnackBar) {
 
   }
   interval: any;
@@ -37,5 +40,10 @@ export class WeatherDisplayComponent implements OnInit {
     this.weatherService.retWea().subscribe((data: WeatherModel) => this.weatherData = data)
   }
 
+  likeFun() {
+    this._snackBar.open('Thanks :)', 'Dismiss', {
+      duration: 3000
+    });
+  }
 
 }
